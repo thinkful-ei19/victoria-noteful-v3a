4c2b3b4d28dc8636c9098e448f65e7e715c91f60
Chris Klanac <cklanac@users.noreply.github.com>
2018-03-20 20:47:05 -0400

    update public

--- public/scripts/noteful.js
+++ public/scripts/noteful.js
@@ -149,7 +149,7 @@ const noteful = (function () {
         id: store.currentNote.id,
         title: editForm.find('.js-note-title-entry').val(),
         content: editForm.find('.js-note-content-entry').val(),
-        folderId: editForm.find('.js-note-folder-entry').val(),
+        folderIid: editForm.find('.js-note-folder-entry').val(),
         tags: editForm.find('.js-note-tags-entry').val()
       };
 
@@ -217,6 +217,7 @@ const noteful = (function () {
         store.currentNote = {};
       }
 
+      console.log('Get notes by folderId, coming soon...');
       api.search('/api/notes', store.currentQuery)
         .then(response => {
           store.notes = response;
@@ -230,6 +231,8 @@ const noteful = (function () {
       event.preventDefault();
 
       const newFolderName = $('.js-new-folder-entry').val();
+
+      console.log('Create a folder, coming soon...');
       api.create('/api/folders', { name: newFolderName })
         .then(() => {
           $('.js-new-folder-entry').val();
@@ -255,15 +258,13 @@ const noteful = (function () {
         store.currentNote = {};
       }
 
+      console.log('Delete a folder, coming soon...');
       api.remove(`/api/folders/${folderId}`)
         .then(() => {
-          const notesPromise = api.search('/api/notes');
-          const folderPromise = api.search('/api/folders');
-          return Promise.all([notesPromise, folderPromise]);
+          return api.search('/api/folders');
         })
-        .then( ([notes, folders])  => {
-          store.notes = notes;
-          store.folders = folders;
+        .then(response => {
+          store.folders = response;
           render();
         });
     });
@@ -279,14 +280,14 @@ const noteful = (function () {
       const tagId = getTagIdFromElement(event.currentTarget);
       store.currentQuery.tagId = tagId;
 
-      //TODO; loop over tags, if not a match, then clear
       store.currentNote = {};
 
-      api.search('/api/notes', store.currentQuery)
-        .then(response => {
-          store.notes = response;
-          render();
-        });
+      console.log('Get notes by tagId, coming soon...');
+      // api.search('/api/notes', store.currentQuery)
+      //   .then(response => {
+      //     store.notes = response;
+      //     render();
+      //   });
     });
   }
 
@@ -295,16 +296,18 @@ const noteful = (function () {
       event.preventDefault();
 
       const newTagName = $('.js-new-tag-entry').val();
-      api.create('/api/tags', { name: newTagName })
-        .then(() => {
-          return api.search('/api/tags');
-        }).then(response => {
-          store.tags = response;
-          render();
-        })
-        .catch(err => {
-          console.error(err);
-        });
+
+      console.log('Create a tag, coming soon...');
+      // api.create('/api/tags', { name: newTagName })
+      //   .then(() => {
+      //     return api.search('/api/tags');
+      //   }).then(response => {
+      //     store.tags = response;
+      //     render();
+      //   })
+      //   .catch(err => {
+      //     console.error(err);
+      //   });
     });
   }
 
@@ -317,24 +320,24 @@ const noteful = (function () {
         store.currentQuery.tagId = null;
       }
 
-      //TODO; loop over tags, if not a match, then clear
       store.currentNote = {};
 
-      api.remove(`/api/tags/${tagId}`)
-        .then(() => {
-          return api.search('/api/tags');
-        })
-        .then(response => {
-          store.tags = response;
-          return api.search('/api/notes', store.currentQuery);
-        })
-        .then(response => {
-          store.notes = response;
-          render();
-        });
+      console.log('Delete a tag, coming soon...');
+      // api.remove(`/api/tags/${tagId}`)
+      //   .then(() => {
+      //     return api.search('/api/tags');
+      //   })
+      //   .then(response => {
+      //     store.tags = response;
+      //     return api.search('/api/notes', store.currentQuery);
+      //   })
+      //   .then(response => {
+      //     store.notes = response;
+      //     render();
+      //   });
     });
   }
-  
+
   function bindEventListeners() {
     handleNoteItemClick();
     handleNoteSearchSubmit();
